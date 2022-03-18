061fc899592b63aebda801e4a2d46253296efdd6
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2022-03-18 10:04:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function red(){
+        document.querySelector('body').style.background = "red";
+        document.querySelector('body').style.color = "white";
+}
+
+function blue(){
+        document.querySelector("body").style.background = "blue";
+        document.querySelector('body').style.color = "white";
+}
+
+function green(){
+        document.querySelector('body').style.background = "green";
+        document.querySelector('body').style.color = 'white';
+}
+
+function pink(){
+        document.querySelector('body').style.background = "pink";
+        document.querySelector('body').style.color = "white";       
+}
+
+function white(){
+        document.querySelector('body').style.background = "white";
+        document.querySelector('body').style.color = "black";
+}
+
+function black(){
+        document.querySelector('body').style.background = "black";
+        document.querySelector('body').style.color = 'white';
+}
